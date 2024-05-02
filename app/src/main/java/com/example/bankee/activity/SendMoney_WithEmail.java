0b2cade07d932058469c5735b3a8ef65ed5229fe
Pronob_Kumnar <pronob2055@gmail.com>
2024-05-02 09:03:36 +0600

    Temporarry Commit

---
+++ app/src/main/java/com/example/bankee/activity/SendMoney_WithEmail.java
@@ -0,0 +1,16 @@
+package com.example.bankee.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bankee.R;
+
+public class SendMoney_WithEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_money_with_email);
+    }
+}
